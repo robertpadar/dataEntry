aa28f739bd2459904a00ef4c323bb121d8930129
Robert Padar <87752792+robertpadar@users.noreply.github.com>
2023-04-21 17:47:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import anime from './anime.js';
+
 $('#button').click(function() {
 	
 	$.ajax({
@@ -15,6 +17,26 @@ $('#button').click(function() {
 				$('#name').html(result[0]['talent'][0]);
 				$('#description').text('no data');
 				$('#submit').text('no data');
+				
+				var textWrapper = document.querySelector('#header');
+				textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+				anime.timeline({loop: true})
+					.add({
+						targets: '#header .letter',
+						scale: [4,1],
+						opacity: [0,1],
+						translateZ: 0,
+						easing: "easeOutExpo",
+						duration: 950,
+						delay: (el, i) => 70*i
+					}).add({
+						targets: '.ml2',
+						opacity: 0,
+						duration: 1000,
+						easing: "easeOutExpo",
+						delay: 1000
+					});
+
 				document.getElementById('header').style.backgroundColor = result[0]['color'];
 				document.getElementById('submit').style.backgroundColor = result[0]['color'];
 				document.getElementById('name').style.backgroundColor = result[0]['color'];
@@ -27,6 +49,26 @@ $('#button').click(function() {
 				$('#name').html(result[1]['talent'][0]);
 				$('#description').html(result[1]['talent'][1]);
 				$('#submit').html(result[1]['talent'][2]);
+				
+				var textWrapper = document.querySelector('#header');
+				textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+				anime.timeline({loop: true})
+					.add({
+						targets: '#header .letter',
+						scale: [4,1],
+						opacity: [0,1],
+						translateZ: 0,
+						easing: "easeOutExpo",
+						duration: 950,
+						delay: (el, i) => 70*i
+					}).add({
+						targets: '.ml2',
+						opacity: 0,
+						duration: 1000,
+						easing: "easeOutExpo",
+						delay: 1000
+					});
+
 				document.getElementById('header').style.backgroundColor = result[1]['color'];
 				document.getElementById('submit').style.backgroundColor = result[1]['color'];
 				document.getElementById('name').style.backgroundColor = result[1]['color'];
@@ -39,6 +81,26 @@ $('#button').click(function() {
 				$('#name').html(result[2]['talent'][0]);
 				$('#description').html(result[2]['talent'][1]);
 				$('#submit').text('no data');
+				
+				var textWrapper = document.querySelector('#header');
+				textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+				anime.timeline({loop: true})
+					.add({
+						targets: '#header .letter',
+						scale: [4,1],
+						opacity: [0,1],
+						translateZ: 0,
+						easing: "easeOutExpo",
+						duration: 950,
+						delay: (el, i) => 70*i
+					}).add({
+						targets: '.ml2',
+						opacity: 0,
+						duration: 1000,
+						easing: "easeOutExpo",
+						delay: 1000
+					});
+
 				document.getElementById('header').style.backgroundColor = result[2]['color'];
 				document.getElementById('submit').style.backgroundColor = result[2]['color'];
 				document.getElementById('name').style.backgroundColor = result[2]['color'];
